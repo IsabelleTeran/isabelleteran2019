ed8c2ecf8a4b7334c1afd80eb1f14197373ab78b
Isabelle Teran <isabelleteran@gmail.com>
2019-01-25 11:40:43 +0100

    miau

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,20 @@ $(document).ready(function(){
 var controller = new ScrollMagic.Controller({globalSceneOptions: {triggerHook: "onEnter", duration: "200%"}});
 						// build scenes
 new ScrollMagic.Scene({triggerElement: "#parallax1"})
-  .setTween("#parallax1 > div", {y: "15%", ease: Linear.easeNone})
+  .setTween("#parallax1 > div", {y: "20%", ease: Linear.easeNone})
   .addTo(controller);
 new ScrollMagic.Scene({triggerElement: "#parallax2"})
-  .setTween("#parallax2 > div", {y: "15%", ease: Linear.easeNone})
+  .setTween("#parallax2 > div", {y: "20%", ease: Linear.easeNone})
   .addTo(controller);
-new ScrollMagic.Scene({triggerElement: "#text1"})
+    new ScrollMagic.Scene({triggerElement: "#parallax3"})
+  .setTween("#parallax3 > div", {y: "20%", ease: Linear.easeNone})
+  .addTo(controller);
+    new ScrollMagic.Scene({triggerElement: "#parallax4"})
+  .setTween("#parallax4 > div", {y: "20%", ease: Linear.easeNone})
+  .addTo(controller);
+/***** new ScrollMagic.Scene({triggerElement: "#text1"})
   .setTween("#text1 > div", {y: "105%", ease: Linear.easeNone})
-  .addTo(controller);
+  .addTo(controller);*********/
 
   })
 
